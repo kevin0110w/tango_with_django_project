5fb2abab888f826e3124d36ef3a8af6f5a5628d2
Kevin Wu <0808148w@student.gla.ac.uk>
2019-01-16 17:12:20 +0000

    customising admin interface

--- rango/admin.py
+++ rango/admin.py
@@ -4,4 +4,8 @@ from rango.models import Category, Page
 # Register your models here.
 
 admin.site.register(Category)
-admin.site.register(Page)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
+admin.site.register(Page, PageAdmin)
